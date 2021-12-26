6ebf8bb128cd1c39dd7f946eeabee45c085bac0a
mark-g-y <mark-g-y@users.noreply.github.com>
2021-12-26 13:54:37 -0500

    Add sprites for airplanes and support rotation.

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,6 +1,6 @@
-// export const WEB_ADDRESS = 'http://localhost:3000';
-export const WEB_ADDRESS = 'https://aerialcombat.y-backend.com';
+export const WEB_ADDRESS = 'http://localhost:3000';
+// export const WEB_ADDRESS = 'https://aerialcombat.y-backend.com';
 export const GAME_WEB_ADDRESS = WEB_ADDRESS + '/game.html';
 
-// export const GAME_SOCKET_ADDRESS = 'ws://localhost:1700';
-export const GAME_SOCKET_ADDRESS = 'wss://y-backend.com:1700';
+export const GAME_SOCKET_ADDRESS = 'ws://localhost:1700';
+// export const GAME_SOCKET_ADDRESS = 'wss://y-backend.com:1700';
