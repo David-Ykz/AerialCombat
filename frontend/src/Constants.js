9bf1447e8c08db3721c6fd265c564a74e2b6b4d4
mark-g-y <mark-g-y@users.noreply.github.com>
2021-12-25 21:38:05 -0500

    Add feature for frontend to hold and shoot.

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,6 +1,6 @@
-// export const WEB_ADDRESS = 'http://localhost:3000';
-export const WEB_ADDRESS = 'https://dogfight.y-backend.com';
+export const WEB_ADDRESS = 'http://localhost:3000';
+// export const WEB_ADDRESS = 'https://dogfight.y-backend.com';
 export const GAME_WEB_ADDRESS = WEB_ADDRESS + '/dogfight.html';
 
-// export const GAME_SOCKET_ADDRESS = 'ws://localhost:1700';
-export const GAME_SOCKET_ADDRESS = 'wss://y-backend.com:1700';
+export const GAME_SOCKET_ADDRESS = 'ws://localhost:1700';
+// export const GAME_SOCKET_ADDRESS = 'wss://y-backend.com:1700';
